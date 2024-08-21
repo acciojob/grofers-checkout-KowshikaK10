98059c67c20dbcccd98d57a14c028c10d5515a8c
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-08-21 16:15:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,18 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	const table = document.querySelector('table');
+  const prices=table.querySelectorAll('.price');
+  let total=0;
+prices.forEach(priceElement => {
+        const priceValue = parseFloat(priceElement.textContent.trim());
+        total += priceValue;
+    });
+	const tr = document.createElement("tr");
+	const td = document.createElement("td");
+	td.append(`Total Price = ${total}`);
+	tr.appendChild(td);
+	table.appendChild(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
