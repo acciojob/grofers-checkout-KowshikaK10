f170e7c82078d450c1c9bf124203cf60bce8128c
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-08-21 16:25:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ const getSum = () => {
 	const table = document.querySelector('table');
   const prices=table.querySelectorAll('.price');
   let total=0;
-prices.forEach(priceElement => {
-        const priceValue = parseFloat(priceElement.textContent.trim());
+for(let i=0;i<prices.length;i++){
+        const priceValue = parseInt(prices[i].textContent);
         total += priceValue;
-    });
+    }
 	const tr = document.createElement("tr");
 	const td = document.createElement("td");
 	td.append(`Total Price = ${total}`);
